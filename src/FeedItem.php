f1a3e3889d1c1879182f33afd5b455d21d50c156
Benjamin Kleiner <bizzl@users.sourceforge.net>
2016-04-13 13:58:26 +0200

    Added base classes and dom parser abstraction

---
+++ src/FeedItem.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * Copyright (c) 2016 Benjamin Kleiner
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+ * documentation files (the "Software"), to deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of
+ * the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+ * WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS
+ * OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+ * OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+namespace Benkle\FeedParser;
+
+
+use Benkle\FeedParser\Interfaces\ItemInterface;
+use Benkle\FeedParser\Traits\WithDescriptionTrait;
+use Benkle\FeedParser\Traits\WithLastModifiedTrait;
+use Benkle\FeedParser\Traits\WithLinkTrait;
+use Benkle\FeedParser\Traits\WithPublicIdTrait;
+use Benkle\FeedParser\Traits\WithTitleTrait;
+
+/**
+ * Class FeedItem
+ * @package Benkle\FeedParser
+ */
+class FeedItem implements ItemInterface, \JsonSerializable
+{
+    use WithPublicIdTrait, WithLastModifiedTrait, WithLinkTrait, WithDescriptionTrait, WithTitleTrait;
+
+    /**
+     * Specify data which should be serialized to JSON
+     * @link http://php.net/manual/en/jsonserializable.jsonserialize.php
+     * @return mixed data which can be serialized by <b>json_encode</b>,
+     * which is a value of any type other than a resource.
+     * @since 5.4.0
+     */
+    function jsonSerialize()
+    {
+        return [
+            'title' => $this->getTitle(),
+            'description' => $this->getDescription(),
+            'lastModified' => $this->getLastModified()->format(\DateTime::ATOM),
+            'publicId' => $this->getPublicId(),
+            'link' => $this->getLink(),
+        ];
+    }
+}
